f85600d3958f1f3e6dd34c23e8ba764b9081efce
davijmc <davi.jose.comc@gmail.com>
2024-10-07 22:18:47 -0300

    Paginas clicaveis

---
+++ script.js
@@ -0,0 +1,13 @@
+const sobre_mim = document.getElementById('sobre_mim');
+const experiencias = document.getElementById('experiencias');
+const projetos = document.getElementById('projetos');
+
+sobre_mim.addEventListener('click', function() {
+    window.location.href = 'sm_p.html';
+});
+experiencias.addEventListener('click', function() {
+    window.location.href = 'ex_p.html';
+});
+projetos.addEventListener('click', function() {
+    window.location.href = 'pj_p.html';
+});
